ebfb7b750f5207c9928eff2518608c2c276d92bd
Leslie Owusu-Appiah <leslie@localhost.international>
2022-05-13 09:19:54 +0100

    feat: post to channels

--- src/components/Share/index.tsx
+++ src/components/Share/index.tsx
@@ -1,53 +1,187 @@
-import { FC } from "react";
-import { Formik, Field, Form, FormikHelpers } from "formik";
+import { FC, ReactFragment, ReactPortal } from "react";
+import { Formik, Form, ErrorMessage, Field, FormikHelpers } from "formik";
+import * as Yup from "yup";
 import styled from "styled-components";
 
 import channelData from "../../data/.test.channels.json";
-import * as Yup from "yup";
 
-interface IShareFormValues {
-  username: string;
-  userComment: string;
-  radarChannel: string;
-}
-interface IRadarChannels {
-  channels: IRadarChannelItem[];
-}
 interface IRadarChannelItem {
   name: string;
   category: string;
   id: string;
-  webhook: URL;
+  webhook: string; //URL;
 }
 
+// interface IRadarChannels {
+//   channels: IRadarChannelItem[];
+// }
 interface IFormValues {
-	username: string;
-	userComment: string;
-	radarChannel: string;
+  username: string;
+  url: string;
+  comment: string;
+  radarChannel: string;
+  date: string;
 }
 
 const { channels } = channelData;
 
-
-
-const validationSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(4, "too short")
-    .max(8, "too long")
-    .required("needed")
-});
-
-/**
- * Yup.object().shape({
- *   username: Yup.string().required().matches(/^.{3,32}#[0-9]{4}$/, "Discord handle required")
- * })}
- */
-
-
 export const Share: FC = () => {
+  const validationSchema = Yup.object({
+    username: Yup.string()
+      .required()
+      .matches(
+        /^.{3,32}#[0-9]{4}$/,
+        "C'mon; that's not a Discord handle/username"
+      )
+      .required("Discord handle required"),
+    url: Yup.string()
+      .required("Add a URL"),
+    radarChannel: Yup.string().required("Choose a channel"),
+    // TODO - Enable the following if multiple options can be selected
+    // channel: Yup.mixed().test("arraySize", "Select at least one channel", (value) => value.length > 0),
+    comment: Yup.string()
+      .min(2, "Too Short!")
+      .max(255, "Too Long!")
+      .required("Add a comment"),
+    date: Yup.date().default(() => new Date()),
+  });
+
+  const initialValues: IFormValues = {
+    username: "",
+    url: "",
+    radarChannel: "", // channels: [], // Question: can you post to multiple channels?
+    comment: "",
+    date: `${new Date()}`,
+  };
+
+  const onSubmitForm = (values: IFormValues) => {
+    setTimeout(() => {
+      const { username, url, radarChannel, comment, date } = values;
+      console.log(
+        JSON.stringify({ username, url, radarChannel, comment, date }, null, 2)
+      );
+      const selectedChannel:any = channels.find(
+        (channel) => channel.id === radarChannel
+      );
+      alert(JSON.stringify(selectedChannel));
+      sendFormDataHook({username: username, url: url, comment: comment, date: date, ...selectedChannel});
+    }, 500);
+  };
+
+  const sendFormDataHook = (formData: { username: string; url: string; comment: string; date: string; name: string; category: string; id: string; webhook: string; }) => {
+
+    const { username, url, comment, date, name, category, id, webhook } = formData
+
+    function listener(this: any) {
+      console.log("Web Hook Repsonse", this.responseText);
+    }
+    const xhrRequest = new XMLHttpRequest();
+    xhrRequest.addEventListener("load", listener);
+    xhrRequest.open(
+      "POST",
+      webhook
+    );
+    xhrRequest.setRequestHeader(
+      "Content-Type",
+      "application/json;charset=UTF-8"
+    );
+    xhrRequest.send(
+      JSON.stringify({
+        username: username, 
+        avatar_url: "", 
+        content: `URL: ${url}\n\nComment: ${comment}\n\nDate: ${date}`
+      }));
+  };
+
+  const renderError = (
+    message: boolean | ReactFragment | ReactPortal | null | undefined
+  ) => <p className="help is-danger">{message}</p>;
+
   return (
     <StyledForm>
       <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={async (values: IFormValues /*, { resetForm }*/) => {
+          onSubmitForm(values);
+          // resetForm()
+        }}
+      >
+        <Form>
+          <div className="field">
+            <label className="label" htmlFor="username">
+              Discord
+            </label>
+            <div className="control">
+              <Field
+                type="text"
+                name="username"
+                placeholder="e.g. LeslieOA#5130"
+              />
+              <ErrorMessage name="username" render={renderError} />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label" htmlFor="url">
+              URL
+            </label>
+            <div className="control">
+              <Field
+                type="text"
+                name="url"
+                placeholder="e.g. https://hypercore-protocol.org/"
+              />
+              <ErrorMessage name="url" render={renderError} />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label" htmlFor="radarChannel">
+              Channel
+            </label>
+            <div className="control">
+              <Field
+                name="radarChannel"
+                as="select"
+                className="select is-fullwidth"
+              >
+                <option value={""} disabled>
+                  Select a channel
+                </option>
+                {channels.map((channel) => {
+                  const { name, id } = channel;
+                  return (
+                    <option key={id} value={id}>
+                      {name}
+                    </option>
+                  );
+                })}
+              </Field>
+              <ErrorMessage name="radarChannel" render={renderError} />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label" htmlFor="comment">
+              Comment
+            </label>
+            <div className="control">
+              <Field
+                component="textarea"
+                name="comment"
+                placeholder="e.g. This website is hosted on IPFS"
+              />
+              <ErrorMessage name="comment" render={renderError} />
+            </div>
+          </div>
+
+          <div className="field button-container">
+            <button type="submit">Share</button>
+          </div>
+        </Form>
+      </Formik>
+
+      {/* <Formik
         initialValues={{
           username: "",
           userComment: "",
@@ -121,7 +255,7 @@ export const Share: FC = () => {
 
           <button type="submit">Share</button>
         </Form>
-      </Formik>
+      </Formik> */}
     </StyledForm>
   );
 };
