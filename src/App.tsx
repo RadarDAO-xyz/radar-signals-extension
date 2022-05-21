5a78592a7422a5e3395b921724ff7e184e6b7c5c
Tal Ben Shoam <talbsho@gmail.com>
2022-05-21 10:41:03 -0400

    first 2 screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,75 @@
+import react, { useState } from "react";
 import styled from "styled-components";
-import { Share } from "./components/Share";
+import { Login } from "./components/Share";
 
+import bg1 from "./assets/images/loginbg.png";
+import bg2 from "./assets/images/bg-image-select-channel.png";
 import radarLogo from "./assets/images/logo.radar.black.svg";
+import { Channel } from "./components/channel";
 
 function App() {
+  const [stage, setStage] = useState("login");
+  const stages = ["login", "share"];
+
+  const nextStage = () => {
+    setStage(stages[(stages.indexOf(stage) + 1) % stages.length]);
+  };
+
+  const renderStage = () => {
+    switch (stage) {
+      case "login":
+        return <Login nextStage={nextStage} />;
+      case "share":
+        return <Channel />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <StyledApp>
-      <header id="header">
-        <h1 className="logo">Radar</h1>
-        <Share />
-      </header>
+      <div
+        className='wrapper'
+        style={{
+          backgroundImage: stage === "login" ? `url(${bg1})` : `url(${bg2})`,
+        }}
+      >
+        <div className='container'>
+          <div className='title'>
+            <img className='logo' src={radarLogo} alt='radar logo' />
+            {stage === "login" && (
+              <div style={{ fontFamily: "WonderType", fontSize: "20px" }}>
+                THE FUTURE OF FUTURES
+              </div>
+            )}
+          </div>
+          {renderStage()}
+        </div>
+      </div>
     </StyledApp>
   );
 }
 
 const StyledApp = styled.div`
+  @font-face {
+    font-family: "WonderType";
+    src: url("/assets/fonts/WonderType-Regular.otf");
+  }
 
-  text-align: center;
+  width: 350px;
+  height: 500px;
+  margin: 0 auto;
 
-  .logo {
-    pointer-events: none;
-    width: 100px;
-    height: 0;
-    padding-top: 18px;
-    background-image: url(${radarLogo});
-    background-size: 100px;
-    background-repeat: no-repeat;
-    display: inline-block;
-    position: relative;
-    overflow: hidden;
+  .title {
+    .logo {
+      width: 135px;
+      height: 29px;
+    }
+    margin-bottom: 60px;
+  }
+
+  h2 {
+    font-size: 16px;
   }
 
   @media (prefers-reduced-motion: no-preference) {
@@ -46,6 +87,40 @@ const StyledApp = styled.div`
     justify-content: center;
     font-size: calc(10px + 1vmin);
     color: black;
+  }
+
+  .wrapper {
+    background-size: cover;
+    padding: 20px;
+    height: 100%;
+    text-align: center;
+    width: 320px;
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 10px;
+      &.container {
+        padding-top: 20px;
+      }
+      button {
+        background: #fff;
+        padding: 22px 0;
+        border: 1px solid #000;
+        cursor: pointer;
+        width: 80%;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        height: 68px;
+        width: 269px;
+        &:active {
+          box-shadow: inset 0 0 3px rgba(0, 0, 0, 0.5);
+        }
+        &.btn-send {
+          background: #000;
+          color: #fff;
+        }
+      }
+    }
   }
 `;
 
--- src/components/channel/index.tsx
+++ src/components/channel/index.tsx
@@ -23,7 +23,7 @@ interface IFormValues {
 
 const { channels } = channelData;
 
-export const Login = ({ nextStage }: { nextStage: () => void }) => {
+export const Channel: FC = () => {
   const initialValues: IFormValues = {
     username: "",
     url: "",
@@ -70,32 +70,28 @@ export const Login = ({ nextStage }: { nextStage: () => void }) => {
 
   return (
     <Wrapper>
-      <button type='button' onClick={nextStage}>
-        <strong>Connect your Discord →</strong>
-      </button>
-      <button>
-        <strong>
-          <a
-            href='https://mirror.xyz/0x149D46eC060e75AE188876AdB6b24024637003C7/UksB2j5ldUAFifjle3w1cGDQCMyHMHCW_9RDSmAhHmw'
-            target='_blank'
-          >
-            Learn more about RADAR →
-          </a>
-        </strong>
-      </button>
-      <button>
-        <strong>
-          <a
-            href='https://discord.com/channels/@me/976638042754408458/977035100791406662'
-            target='_blank'
-          >
-            How to use this plugin →
-          </a>
-        </strong>
+      <h2>Why is this signal interesting?</h2>
+      <textarea placeholder='Write a comment...' />
+      <button type='button'>Select Channel ↓</button>
+      <button className='btn-send' type='button'>
+        SEND
       </button>
     </Wrapper>
   );
 };
 
 // TODO - Move to ./style as SC
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  textarea {
+    background: #fff;
+    padding: 22px;
+    border: 1px solid #000;
+    width: 269px;
+    height: 100px;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    resize: none;
+    &:active {
+      box-shadow: inset 0 0 3px rgba(0, 0, 0, 0.5);
+    }
+  }
+`;
